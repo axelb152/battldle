c9c4b7517f4615694da706462d53279a0615ad13
Axel Bagenal <axel.bagenal@gmail.com>
2023-10-09 22:47:49 +0200

    update guess emoji

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -166,7 +166,7 @@ export function Game({ settingsData }: GameProps) {
                 className="border-2 uppercase my-0.5 hover:bg-gray-50 active:bg-gray-100 dark:hover:bg-slate-800 dark:active:bg-slate-700"
                 type="submit"
               >
-                🌍 {t("guess")}
+                ⚔️ {t("guess")}
               </button>
             </div>
           </form>
